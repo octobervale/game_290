3bff47691fba801392e953d25e9afdc124f4d5e2
octobervale <valesmith@u.boisestate.edu>
2024-04-10 22:42:14 -0600

    added game code
    added game code

---
+++ Unity1-290/Assets/scripts/SceneController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    private GameObject enemy;
+    void Update()
+    {
+
+        if (enemy == null)
+        {
+             float randomHeight = Random.Range(1f, 3f);
+            enemy = Instantiate(enemyPrefab) as GameObject;
+            enemy.transform.localScale = new Vector3(1f, randomHeight, 1f);
+            enemy.transform.position = new Vector3(0, 1, 0);
+            float angle = Random.Range(0, 360);
+            enemy.transform.Rotate(0, angle, 0);
+            Renderer renderer = enemy.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material.color = Random.ColorHSV();
+            }
+        }
+    }
+}
---
+++ Unity1-290/Assets/scripts/fireball.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class fireball : MonoBehaviour {
+ public float speed = 10.0f;
+ public int damage = 1;
+ void Update() {
+ transform.Translate(0, 0, speed * Time.deltaTime);
+ }
+ void OnTriggerEnter(Collider other) {
+ PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+ if (player != null) {
+ player.Hurt(damage);
+ }
+ Destroy(this.gameObject);
+ }
+}
